1c2fe73cb61f6aa416715b0f3b6d813e85404567
lhspirit666 <3408395015@qq.com>
2024-03-14 19:14:40 +0800

    lab1

--- dag.go
+++ dag.go
@@ -1,47 +1,92 @@
 package merkledag
 
-import "hash"
+import (
+	"encoding/json"
+	"hash"
+)
+
+type Link struct {
+	Name string
+	Hash []byte
+	Size int
+}
+
+type Object struct {
+	Links []Link
+	Data  []byte
+}
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	switch n := node.(type) {
-	case File:
-		// 对文件节点进行哈希计算
-		hashBytes := n.Bytes()
-		h.Reset()
-		h.Write(hashBytes)
-		hashValue := h.Sum(nil)
-
-		// 将哈希值存入 KVStore
-		if err := store.Put(hashValue, hashBytes); err != nil {
-			panic(err) // 错误处理可以更加健壮
-		}
-
-		return hashValue
-
-	case Dir:
-		// 对目录节点进行哈希计算
-		it := n.It()
-		buf := make([]byte, 0)
-		for it.Next() {
-			childNode := it.Node()
-			childHash := Add(store, childNode, h)
-			// 将子节点的哈希值写入缓冲区
-			buf = append(buf, childHash...)
-		}
-		// 计算目录节点的哈希值
-		h.Reset()
-		h.Write(buf)
-		hashValue := h.Sum(nil)
-
-		// 将哈希值存入 KVStore
-		if err := store.Put(hashValue, buf); err != nil {
-			panic(err) // 错误处理可以更加健壮
-		}
-
-		return hashValue
-
-	default:
-		panic("Unknown node type")
+	switch node.Type() {
+	case DIR:
+		file := node.(File)
+		tmp := StoreFile(store, file, h)
+		jsonMarshal, _ := json.Marshal(tmp)
+		hash := calculateHash(jsonMarshal, h)
+		return hash
+	case FILE:
+		dir := node.(Dir)
+		tmp := StoreDir(store, dir, h)
+		jsonMarshal, _ := json.Marshal(tmp)
+		hash := calculateHash(jsonMarshal, h)
+		return hash
 	}
-	return nil
+	panic("unknown node")
+}
+
+func calculateHash(data []byte, h hash.Hash) []byte {
+	h.Reset()
+	hash := h.Sum(data)
+	h.Reset()
+	return hash
+}
+
+func StoreFile(store KVStore, file File, h hash.Hash) *Object {
+	data := file.Bytes()
+	blob := Object{Data: data, Links: nil}
+	jsonMarshal, _ := json.Marshal(blob)
+	hash := calculateHash(jsonMarshal, h)
+	store.Put(hash, data)
+	return &blob
+}
+
+func StoreDir(store KVStore, dir Dir, h hash.Hash) *Object {
+	it := dir.It()
+	treeObject := &Object{}
+	for it.Next() {
+		n := it.Node() 
+		switch n.Type() {
+		case FILE:
+			file := n.(File)
+			tmp := StoreFile(store, file, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			hash := calculateHash(jsonMarshal, h)
+			treeObject.Links = append(treeObject.Links, Link{
+				Hash: hash,
+				Size: int(file.Size()),
+				Name: file.Name(),
+			})
+			typeName := "link"
+			if tmp.Links == nil {
+				typeName = "blob"
+			}
+			treeObject.Data = append(treeObject.Data, []byte(typeName)...)
+		case DIR:
+			dir := n.(Dir)
+			tmp := StoreDir(store, dir, h)
+			jsonMarshal, _ := json.Marshal(tmp)
+			hash := calculateHash(jsonMarshal, h)
+			treeObject.Links = append(treeObject.Links, Link{
+				Hash: hash,
+				Size: int(dir.Size()),
+				Name: dir.Name(),
+			})
+			typeName := "tree"
+			treeObject.Data = append(treeObject.Data, []byte(typeName)...)
+		}
+	}
+	jsonMarshal, _ := json.Marshal(treeObject)
+	hash := calculateHash(jsonMarshal, h)
+	store.Put(hash, jsonMarshal)
+	return treeObject
 }
